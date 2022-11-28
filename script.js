099510f1342dfc93512f46c25c70b912e35b2229
Barbara Viana <81521722+BarbaraViana@users.noreply.github.com>
2022-11-28 19:31:15 -0300

    up
    *

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
 const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.querySelector('[date-board');
 
-let isCircleTurn;
+let isCircleTurn
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, { once: true })
+    }
+    isCircleTurn = false;
+    board.classList.add('x');
+}
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentPlayer)
+        })
+    })
+
+};
+
 
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd);
 
 }
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn
 
-const handleClick = () => {
+    board.classList.remove('circle')
+    board.classList.remove('x')
+
+    if (isCircleTurn) {
+        board.classList.add('circle')
+
+    } else {
+        board.classList.add('x')
+    }
+}
+
+const handleClick = (e) => {
     //colocar a marca X ou circulo
     const cell = e.target;
-    const classToAdd = isCircleturn ? 'circle' : 'x';
+    const classToAdd = isCircleTurn ? 'circle' : 'x';
+
+    cell.classList.add(classToAdd)
     placeMark(cell, classToAdd)
         //verificar por vitória
         // verficar por empate
         //mudar simbolo
 
-}
+    swapTurns();
 
-for (const cell of cellElements) {
-    cell.addEventListener("click", handleClick, { once: true })
-}
+
+};
+startGame()
